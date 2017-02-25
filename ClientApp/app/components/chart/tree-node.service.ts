61f5ad8d5131ab5047c131d37fde3b81673c0339
Andres Ward <ajward06@outlook.com>
2017-02-25 12:14:57 -0600

    Create service to access static tree test data

---
+++ ClientApp/app/components/chart/tree-node.service.ts
@@ -0,0 +1,11 @@
+﻿import { Injectable } from "@angular/core";
+import { TREEDATA } from "./mock-tree-nodes";
+import { TreeNode } from "./tree-node";
+import Promise from "ts-promise";
+
+@Injectable()
+export class TreeNodeService {
+    getTreeNodes(): Promise<TreeNode> {
+        return Promise.resolve(TREEDATA);
+    }
+}
