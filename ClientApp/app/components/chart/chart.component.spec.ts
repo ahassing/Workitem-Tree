54aded5b8a773745c684ee66440f7c2629477ea3
Andres Ward <ajward06@outlook.com>
2017-03-05 22:03:55 -0600

    Add chart component spec file template

---
+++ ClientApp/app/components/chart/chart.component.spec.ts
@@ -0,0 +1,31 @@
+﻿import {} from '@types/jasmine';
+
+describe('In the file chart.component.ts', () => {
+    describe('the chart.components\'s', () => {
+        
+        beforeEach(() => {          
+        });
+
+        describe('constructor', () => {
+
+        });
+        describe('getTreeData', () => {
+
+        });
+        describe('collapse', () => {
+
+        });
+        describe('update', () => {
+
+        });
+        describe('nodeClick', () => {
+
+        });
+        describe('redraw', () => {
+
+        });
+        describe('initTree', () => {
+
+        });
+    });
+})
