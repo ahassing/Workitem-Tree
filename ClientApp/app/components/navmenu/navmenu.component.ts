c58c5218384d950bdf4d77d2143f3b13c9b70856
pgayeeL4 <pgayee@gmail.com>
2017-03-09 17:39:41 -0600

    moved navbar color from html to css styling in component

--- ClientApp/app/components/navmenu/navmenu.component.ts
+++ ClientApp/app/components/navmenu/navmenu.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
         .nav.navbar-nav {font-size: 15px;}
         #searchForm {margin-right: 100px;}
         @media (max-width: 1200px) {#searchForm {display:none}}
-        #navigationbar { padding-left: 10px; padding-right: 10px;}
+        #navigationbar { padding-left: 10px; padding-right: 10px; background-color: #13a9e1;}
     `]
 })
 export class NavMenuComponent {
