ad10f9cb46899d50acf62feec532a93007f18d0f
Andres Ward <ajward06@outlook.com>
2017-03-30 00:02:50 -0500

    Step towards getting Karma to work

--- ClientApp/app/components/chart/chart.component.spec.ts
+++ ClientApp/app/components/chart/chart.component.spec.ts
@@ -1,15 +1,33 @@
-﻿import {} from '@types/jasmine';
+﻿import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ChartComponent } from './chart.component';
+import { TreeModalComponent } from '../Modal/modal.component';
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { TreeNode } from './tree-node';
 import { TREEDATA } from './mock-tree-nodes';
 
-describe('In the file chart.component.ts', () => {
-    describe('the chart.components\'s', () => {
+describe('ChartComponent', () => {
+    let component: ChartComponent;
+    let fixture: ComponentFixture<ChartComponent>;
 
-        let testData = {};
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [ChartComponent, TreeModalComponent, ModalComponent],
+        }).compileComponents();
+    }));
 
-        beforeEach(() => {          
-            testData = TREEDATA;
-        });
-
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ChartComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-})
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+});
