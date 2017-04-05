4987f8f5459cef4fdddb83d1798760c805c63a37
jknust <jared.knust@gmail.com>
2017-04-05 16:25:35 -0500

    Heroku fix. Hopefully...

--- ClientApp/app/components/chart/chart.component.ts
+++ ClientApp/app/components/chart/chart.component.ts
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 import 'bootstrap';
-import { TreeModalComponent } from '../Modal/modal.component';
+import { TreeModalComponent } from '../modal/modal.component';
 import { TreeNode } from "./tree-node";
 import { TreeNodeService } from './tree-node.service';
 import { Project } from '../projectlist/project';
